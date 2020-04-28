df7a2d5f9b009a20589c686ffba9b359a7731150
caoshanshan <1480430521@qq.com>
2020-04-28 21:54:34 +0800

    add getCity

--- src/components/CiList/index.vue
+++ src/components/CiList/index.vue
@@ -1,5 +1,7 @@
 <template>
 <div class="cinema_body">
+		<Loading v-if="isLoading"/>
+			<Scroller v-else>
 				<ul>
 					<!--<li>
 						<div>
@@ -29,6 +31,7 @@
        					</div>
 					</li>
 				</ul>
+			</Scroller>
 			</div>
 </template>
 <script>
@@ -36,14 +39,22 @@ export default {
     name:'CiList',
 	data(){
 		return{
-			cinemaList:[]
+			cinemaList:[],
+			isLoading:true,
+			prevCityId:-1
 		}
 	},
 	mounted() {
+		var cityId=this.$store.state.city.id;
+		if(this.prevCityId==cityId){return;}
+		this.isLoading=true;
+
 		this.axios.get('/api/cinemaList?cityId=10').then((res)=>{
 			var msg=res.data.msg;
 			if(msg==='ok'){
 				this.cinemaList=res.data.data.cinemas;
+				this.isLoading=false;
+				this.prevCityId=cityId;
 			}
 		})
 	},
